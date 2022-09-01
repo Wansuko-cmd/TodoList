1f2e889ff41dcde3ca94d930a02bff01c6365d4c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-09-01 18:45:25 +0900

    [format] ktlintを実行

--- ui/src/main/kotlin/com/wsr/ui/memo/index/component/MemoIndexMemoTile.kt
+++ ui/src/main/kotlin/com/wsr/ui/memo/index/component/MemoIndexMemoTile.kt
@@ -10,9 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.wsr.theme.TodoListTheme
 import com.wsr.ui.memo.index.MemoIndexMemoUiState
 
 @Composable
