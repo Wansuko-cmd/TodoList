8b9bc1ecb8412d41a2f2119ce1495a2f9441b57c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-12-26 23:11:54 +0900

    [fix] モーダルの見た目を修正

--- ui/src/main/kotlin/com/wsr/ui/memo/index/component/MemoIndexCreateMemoDialog.kt
+++ ui/src/main/kotlin/com/wsr/ui/memo/index/component/MemoIndexCreateMemoDialog.kt
@@ -1,24 +1,19 @@
 package com.wsr.ui.memo.index.component
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.wsr.theme.MossGreen
+import com.wsr.theme.TodoListTheme
 import com.wsr.ui.R
 
 @Composable
@@ -32,7 +27,7 @@ fun MemoIndexCreateMemoDialog(
         Surface {
             Column(
                 modifier = modifier
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 24.dp)
                     .padding(top = 16.dp),
             ) {
                 Text(
@@ -43,26 +38,60 @@ fun MemoIndexCreateMemoDialog(
                 Spacer(modifier = Modifier.height(16.dp))
 
                 OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
                     value = text,
                     onValueChange = setText,
                     placeholder = { Text(text = stringResource(id = R.string.memo_index_create_memo_title)) },
                 )
 
+                Spacer(modifier = Modifier.height(16.dp))
+
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End,
                 ) {
-                    TextButton(onClick = onDismiss) {
-                        Text(text = stringResource(id = R.string.memo_index_create_memo_dismiss_button))
-                    }
-                    TextButton(
-                        onClick = { onConfirm(text) },
-                        enabled = text.isNotBlank(),
+                    Button(
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = MossGreen,
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                        onClick = onDismiss,
                     ) {
-                        Text(text = stringResource(id = R.string.memo_index_create_memo_confirm_button))
+                        Text(
+                            color = Color.White,
+                            text = stringResource(id = R.string.memo_index_create_memo_dismiss_button),
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = Color(0xFF0099CC)
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                        onClick = { onConfirm(text) },
+                    ) {
+                        Text(
+                            color = Color.White,
+                            text = stringResource(id = R.string.memo_index_create_memo_confirm_button),
+                        )
                     }
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun MemoIndexCreateMemoDialogPreview() {
+    TodoListTheme {
+        MemoIndexCreateMemoDialog(
+            onDismiss = {},
+            onConfirm = {},
+        )
     }
 }
