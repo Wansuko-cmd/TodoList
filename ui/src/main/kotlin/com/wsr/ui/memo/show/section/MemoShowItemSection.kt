e857c82515283c58bbbc2eb7552878ceffc602c6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-04-19 14:45:43 +0900

    [add] 入れ替え機能を追加

--- ui/src/main/kotlin/com/wsr/ui/memo/show/section/MemoShowItemSection.kt
+++ ui/src/main/kotlin/com/wsr/ui/memo/show/section/MemoShowItemSection.kt
@@ -1,28 +1,27 @@
 package com.wsr.ui.memo.show.section
 
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.layout.Spacer
+import android.annotation.SuppressLint
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.wsr.memo.ItemContent
 import com.wsr.memo.ItemId
+import com.wsr.ui.memo.show.MemoShowItemUiState
 import com.wsr.ui.memo.show.MemoShowUiState
 import com.wsr.ui.memo.show.component.MemoShowItemTile
 
+@SuppressLint("FrequentlyChangingValue")
 @Composable
 fun MemoShowItemSection(
     modifier: Modifier = Modifier,
@@ -31,35 +30,62 @@ fun MemoShowItemSection(
     onChangeContent: (itemId: ItemId, content: ItemContent) -> Unit,
     onMoveItem: (from: ItemId, to: ItemId) -> Unit,
 ) {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        // itemの移動時の挙動を直すために必要
-        item {
-            Spacer(modifier = Modifier.height(4.dp))
-        }
+    val state = rememberLazyListState()
+    var currentIndex: Int? by remember { mutableStateOf(null) }
+    LazyColumn(
+        state = state,
+        modifier = modifier
+            .fillMaxSize()
+            .pointerInput(state.layoutInfo.visibleItemsInfo.size) {
+                detectDragGesturesAfterLongPress(
+                    onDragStart = { offset ->
+                        currentIndex = state.layoutInfo
+                            .visibleItemsInfo
+                            .find { info ->
+                                offset.y.toInt() in info.offset..info.offset + info.size
+                            }
+                            ?.index
+                    },
+                    onDragEnd = {
+                        currentIndex = null
+                    },
+                    onDrag = { change, dragAmount ->
+                        change.consume()
+                        val current = currentIndex
+                            ?.let { state.layoutInfo.visibleItemsInfo.getOrNull(it) }
+                            ?: return@detectDragGesturesAfterLongPress
 
-        items(uiState.items, key = { it.id.value }) { item ->
-            var offset by remember { mutableStateOf(Offset.Zero) }
+                        val target = if (dragAmount.y > 0f) {
+                            state.layoutInfo
+                                .visibleItemsInfo
+                                .dropLast(current.index)
+                                .find { change.position.y < it.offset + it.size / 2 }
+                        } else {
+                            state.layoutInfo
+                                .visibleItemsInfo
+                                .drop(current.index)
+                                .find { change.position.y > it.offset - it.size / 2 }
+                        }
+
+                        if (target == null) return@detectDragGesturesAfterLongPress
+                        val from = ItemId(current.key.toString())
+                        val to = ItemId(target.key.toString())
+                        onMoveItem(from, to)
+                        currentIndex = target.index
+                    },
+                )
+            },
+    ) {
+        itemsIndexed(
+            items = uiState.items,
+            key = { _, item -> item.id.value },
+        ) { index: Int, item: MemoShowItemUiState ->
             MemoShowItemTile(
                 modifier = Modifier
                     .animateItem()
-                    .padding(vertical = 8.dp, horizontal = 16.dp)
-                    .pointerInput(Unit) {
-                        detectVerticalDragGestures(
-                            onDragStart = {},
-                            onDragEnd = {
-                                offset = Offset.Zero
-                            },
-                            onDragCancel = {},
-                            onVerticalDrag = { change, _ ->
-                                offset = change.position
-                            },
-                        )
-                    }
-                    .graphicsLayer {
-                        translationY = offset.y
-                    },
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
                 itemUiState = item,
-                isDragging = false,
+                isDragging = index == currentIndex,
                 shouldFocus = item.id == uiState.shouldFocusItemId,
                 onChangeChecked = onChangeChecked,
                 onChangeContent = onChangeContent,
